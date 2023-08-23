a20c3706ec97d91b45d45f4aa4cc05a889a170d7
Stanislav M. Ivankin <lessgrep@gmail.com>
2023-08-23 23:29:27 +0300

    Script for connection/disconnecting monitor on-fly. Using udev rules

---
+++ monitor-switch.sh
@@ -0,0 +1,23 @@
+#!/bin/env bash
+
+if [ "$1" != "forked" ]; then
+    echo "$(dirname $0)/$(basename $0) forked" | at now
+    exit
+fi
+
+export DISPLAY=:0
+export XAUTHORITY=/home/grepz/.Xauthority
+
+card_path=/sys/$(udevadm info -q path -n /dev/dri/card0)
+con_mon=$(xrandr | awk -e '$0 ~ /[^eDP1][\w0-9-]+ connected/ {print $1}')
+card_path=/sys/$(udevadm info -q path -n /dev/dri/card0)
+
+DT=$(date)
+
+echo "$DT Monitor connected: '$con_mon'" >> /tmp/udev.echo
+
+if [ -n "$con_mon" ]; then
+    xrandr --output eDP1 --auto --output $con_mon --auto --right-of eDP1
+else
+    xrandr --output eDP1 --auto
+fi
