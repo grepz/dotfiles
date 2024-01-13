fd9c3b852986738020694113de85bc47fce0069e
Stanislav M. Ivankin <lessgrep@gmail.com>
2024-01-13 14:00:08 +0200

    Minor changes to monitor script + adding volume script

--- monitor-switch.sh
+++ monitor-switch.sh
@@ -1,9 +1,5 @@
 #!/bin/env bash
 
-## NOTE: File should be used in udev rules:
-## $ cat /etc/udev/rules.d/90-monitor.rules
-## KERNEL=="card0", ACTION=="change", SUBSYSTEM=="drm", RUN+="/path/to/monitor-switch.sh"
-
 if [ "$1" != "forked" ]; then
     echo "$(dirname $0)/$(basename $0) forked" | at now
     exit
@@ -12,8 +8,7 @@ fi
 export DISPLAY=:0
 export XAUTHORITY=/home/grepz/.Xauthority
 
-card_path=/sys/$(udevadm info -q path -n /dev/dri/card0)
-con_mon=$(xrandr | awk -e '$0 ~ /[^eDP1][\w0-9-]+ connected/ {print $1}')
+con_mon=$(xrandr | awk -e '$0 ~ /[^(eDP1)][\w0-9-]* connected/ {print $1}')
 card_path=/sys/$(udevadm info -q path -n /dev/dri/card0)
 
 DT=$(date)
